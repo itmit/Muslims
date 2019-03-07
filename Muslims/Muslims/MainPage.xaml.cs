1c2f946f07feaf677324d35224aa09ce61c6df3d
AnimuAbysswalker <demonabigor@mail.ru>
2019-03-07 14:21:52 +0300

    first commit

---
+++ Muslims/Muslims/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Muslims
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void init()
+        {
+            Detail = new NavigationPage(new News());
+        }
+    }
+}
